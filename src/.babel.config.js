09140bf1b0f70435c817991b1c54d6f37fe8f3cb
Lee Wilson <leevincentwilson@gmail.com>
2019-01-13 16:43:24 +0000

    Improved build process and removed Jest Enzyme config as default due to babel conflict issues

--- src/.babel.config.js
+++ src/.babel.config.js
@@ -2,5 +2,11 @@ module.exports = () => ({
   presets: [
     "@babel/env",
     "@babel/preset-react"
+  ],
+  plugins:[
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose" : true }],
+    "add-react-displayname",
+    "@babel/plugin-proposal-object-rest-spread"
   ]
 });
